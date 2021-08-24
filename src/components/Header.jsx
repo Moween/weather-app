c6f1332b961d87be01093309715018314dcdd56c
moween <moween1998@gmail.com>
2021-08-24 15:34:21 +0100

    Create Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return ( 
+    <header className="header container-fluid">
+      <h1>Weather Forcast</h1>
+    </header>
+  );
+}
+ 
+export default Header;
