43ecfe113a5f7b634e3ddf4480b871dde9858fea
moween <moween1998@gmail.com>
2021-08-24 16:05:14 +0100

    Destructure Props

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 
 const Form = (props) => {
+  const { onSubmit, onChange } = props;
   return (
-    <form className="d-flex">
+    <form className="d-flex" onSubmit={onSubmit}>
       <input
         className="form-control me-2"
         name="search"
         type="search"
         placeholder="Search"
         aria-label="Search"
+        onChange={onChange}
       />
       <button className="btn btn-outline-success" type="submit">
         Search
