fb433da3dbd684c4d47b2ea9961bdbad639283d0
moween <moween1998@gmail.com>
2021-08-24 16:06:10 +0100

    Add class methods

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Form from "./Form";
 
 class Main extends Component {
   constructor(props) {
     super(props);
-    this.state = {  };
+    this.state = {
+      search: "",
+      location: {}
+    };
   }
-  
-  render() { 
+
+  handleChange = ({ target: input}) => {
+    const { name, value } = input
+    this.setState({ [name]: value })
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const location = {
+      city: this.state.search,
+    }
+    this.setState({ location });
+  };
+
+  render() {
+    const { search } = this.state;
     return (
       <main className="container">
         <div>
+          <Form
+            search={search}
+            onSubmit={this.handleSubmit}
+            onChange={this.handleChange}
+          />
         </div>
       </main>
     );
   }
 }
- 
+
 export default Main;
