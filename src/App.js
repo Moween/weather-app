9862bdf81ba065ca958f040ce2b2bff9e68ba040
moween <moween1998@gmail.com>
2021-08-30 10:06:54 +0100

    Import react-toastify css.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import './App.css';
-import Header from './components/Header';
-import Main from './components/Main';
+import "./App.css";
+import 'react-toastify/dist/ReactToastify.css'
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
   return (
