3454e5401d40fff24c4099ac97632ee3b1cf2f57
moween <moween1998@gmail.com>
2021-08-31 11:16:44 +0100

    Add page footer with copyright details

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return ( 
+    <footer>
+      <div className="limited">
+        <p>Designed by &copy;Orbie 2021</p>
+      </div>
+    </footer>
+   );
+}
+ 
+export default Footer;
