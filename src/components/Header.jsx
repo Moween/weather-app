a8d676e04567535c1b5947210830f2454f8ae1f1
moween <moween1998@gmail.com>
2021-08-31 09:19:39 +0100

    Add date

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 const Header = () => {
+  const date = new Date().toDateString();
   return (
     <header className="header container-fluid">
       <div className="limited">
         <h1>Today's Weather</h1>
+        <div>
+          <p id="date">{date}</p>
+        </div>
       </div>
     </header>
   );
