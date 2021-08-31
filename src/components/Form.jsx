965ca6bb74ef60f72be8013ae02058c295d6c6dd
moween <moween1998@gmail.com>
2021-08-31 11:19:46 +0100

    Reposition submit-button Add Search Icon.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,22 +1,25 @@
 import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
 
 const Form = (props) => {
-  const { onSubmit, onChange , value} = props;
+  const { onSubmit, onChange, value } = props;
   return (
     <form id="form" className="d-flex" onSubmit={onSubmit}>
-      <input
-        className="form-control me-2"
-        name="search"
-        type="search"
-        placeholder="search  city"
-        aria-label="Search"
-        onChange={onChange}
-        required
-        value={value}
-      />
-      <button className="btn btn-light" type="submit">
-        Search
-      </button>
+      <div>
+        <button className="submit-btn" type="submit">
+          <SearchIcon className="fa fa-search" />
+        </button>
+        <input
+          className="searchbar"
+          name="search"
+          type="search"
+          placeholder="search city..."
+          aria-label="Search"
+          onChange={onChange}
+          required
+          value={value}
+        />
+      </div>
     </form>
   );
 };
